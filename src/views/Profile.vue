b6d02f91d66ec3489c41baac91e444e338a7a042
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-22 22:32:05 +0700

    Update Profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,27 +1,44 @@
 <template>
-    <div>
-    <v-subheader>Your Profile</v-subheader>    
-    <v-card flat>
-        <v-container>
-            <v-simple-table>
-            <tbody>
+  <div>
+    <v-subheader>Your Profile</v-subheader>
+
+    <v-card flat v-if="user">
+      <v-container>
+        <v-simple-table>
+          <tbody>
             <tr v-for="(value, key) in user" :key="key">
-                <td>{{ key }}</td>
-                <td>{{ value }}</td>
+              <td class="font-weight-bold">{{ formatKey(key) }}</td>
+              <td>{{ value || '-' }}</td>
             </tr>
-            </tbody>
-            </v-simple-table>
-        </v-container>
+          </tbody>
+        </v-simple-table>
+      </v-container>
     </v-card>
-    </div>
+
+    <!-- Skeleton Loader saat data belum siap -->
+    <v-card flat v-else>
+      <v-container>
+        <v-skeleton-loader type="table-row"></v-skeleton-loader>
+      </v-container>
+    </v-card>
+  </div>
 </template>
+
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
+
 export default {
-    computed: {
-      ...mapGetters({
-        'user':'auth/user'
-      }),
-    }
-}
+  computed: {
+    ...mapGetters({
+      user: "auth/user",
+    }),
+  },
+  methods: {
+    formatKey(key) {
+      return key
+        .replace(/_/g, " ") // Ganti underscore dengan spasi
+        .replace(/\b\w/g, (char) => char.toUpperCase()); // Kapitalisasi huruf pertama
+    },
+  },
+};
 </script>
