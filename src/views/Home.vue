8c1f91a9e64e6029a0b5cc7ed92edebd9662ee0f
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-22 22:31:33 +0700

    Updaet Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,72 +1,79 @@
 <template>
   <div>
+    <!-- Categories -->
     <v-container class="ma-0 pa-0" grid-list-sm>
       <div class="text-right">
         <v-btn small text to="/categories" class="blue--text">
           All Categories <v-icon>mdi-chevron-right</v-icon>
         </v-btn>
-      </div> 
+      </div>
       <v-layout wrap>
-        <v-flex v-for="(category) in categories" :key="`category-`+category.id" xs6>
-          <v-card :to="'/category/'+ category.slug">
-            <v-img
-              :src="getImage('/categories/'+category.image)"
-              class="white--text"
-            >
-              <v-card-title 
-                class="fill-height align-end"
-                v-text="category.name"
-              ></v-card-title>
+        <v-flex v-for="category in categories" :key="`category-` + category.id" xs6>
+          <v-card :to="'/category/' + category.slug">
+            <v-img :src="getImage(category.image)" class="white--text">
+              <v-card-title class="fill-height align-end">{{ category.name }}</v-card-title>
             </v-img>
           </v-card>
         </v-flex>
       </v-layout>
     </v-container>
 
+    <!-- Books -->
     <v-container class="ma-0 pa-0 mt-2" grid-list-sm>
       <div class="text-right">
         <v-btn small text to="/books" class="blue--text">
           All Books <v-icon>mdi-chevron-right</v-icon>
         </v-btn>
-      </div> 
+      </div>
       <v-layout wrap>
-        <v-flex v-for="(book) in books" :key="`book-`+book.id" xs6>
+        <v-flex v-for="book in books" :key="`book-` + book.id" xs6>
           <book-item :book="book" />
         </v-flex>
       </v-layout>
+    </v-container>
+
+    <!-- Loading Indicator -->
+    <v-container v-if="loading" class="text-center">
+      <v-progress-circular indeterminate color="blue"></v-progress-circular>
     </v-container>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   components: {
-    BookItem: () => import(/* webpackChunkName: "book-item" */ '@/components/BookItem.vue')
+    BookItem: () => import("@/components/BookItem.vue"),
   },
-  data: () => ({
-    categories: [],
-    books: []
-  }),
-  created(){
-    this.axios.get('/categories/random/2')
-      .then((response) => {
-          let { data } = response.data
-          this.categories = data
-      })
-      .catch((error) => {
-          let { responses } = error
-          console.log(responses)
-      })
-    
-    this.axios.get('/books/top/4')
-      .then((response) => {
-          let { data } = response.data
-          this.books = data
-      })
-      .catch((error) => {
-          let { responses } = error
-          console.log(responses)
-      })
-  }
+  data() {
+    return {
+      categories: [],
+      books: [],
+      loading: false, // Tambahkan state loading
+    };
+  },
+  async created() {
+    this.loading = true;
+    try {
+      // Jalankan kedua request bersamaan agar lebih cepat
+      const [categoriesRes, booksRes] = await Promise.all([
+        axios.get("/categories/random/2"),
+        axios.get("/books/top/4"),
+      ]);
+
+      this.categories = categoriesRes.data.data;
+      this.books = booksRes.data.data;
+    } catch (error) {
+      console.error("Error fetching data:", error.response?.data || error.message);
+    } finally {
+      this.loading = false; // Matikan loading setelah request selesai
+    }
+  },
+  methods: {
+    getImage(image) {
+      return `/categories/${image}`;
+    },
+  },
 };
 </script>
