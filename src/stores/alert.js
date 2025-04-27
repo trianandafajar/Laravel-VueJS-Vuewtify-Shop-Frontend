d971d45402052f9b2c2aa036d911ce399d21a6bd
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-27 23:37:39 +0700

    Update Alert

--- src/stores/alert.js
+++ src/stores/alert.js
@@ -1,25 +1,29 @@
 export default {
-    namespaced: true,
-    state: {
-      status  : false,
-      color    : 'success', // warning, error
-      text    : ''
+  namespaced: true,
+  
+  state: () => ({
+    status: false,
+    color: 'success', // options: success, warning, error
+    text: '',
+  }),
+  
+  mutations: {
+    set(state, { status, text, color }) {
+      state.status = status;
+      state.text = text;
+      state.color = color;
     },
-    mutations: {
-      set: (state, payload) => {
-          state.status  = payload.status
-          state.text    = payload.text
-          state.color    = payload.color
-      },
+  },
+
+  actions: {
+    set({ commit }, payload) {
+      commit('set', payload);
     },
-    actions: {
-      set: ({commit}, payload) => {
-          commit('set', payload)
-      },
-    },
-    getters: {
-      status  : state => state.status,
-      color   : state => state.color,
-      text    : state => state.text
-    }
-} 
+  },
+
+  getters: {
+    status: state => state.status,
+    color: state => state.color,
+    text: state => state.text,
+  },
+};
