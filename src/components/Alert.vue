27e676180bdc195fd6ed029208c83a357eadc083
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-27 23:38:08 +0700

    Update Compinent Alert

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,43 +1,43 @@
 <template>
   <v-snackbar v-model="alert" :color="color" multi-line top>
-      {{ text }}
-      <v-btn dark text @click="close">
-        <v-icon>mdi-close-circle</v-icon>
-      </v-btn>
+    {{ text }}
+    <v-btn dark text @click="close">
+      <v-icon>mdi-close-circle</v-icon>
+    </v-btn>
   </v-snackbar>
 </template>
+
 <script>
 import { mapGetters, mapActions } from 'vuex'
+
 export default {
-    name: 'alert',
-    computed: {
-      ...mapGetters({
-        status : 'alert/status',
-        color  : 'alert/color',
-        text   : 'alert/text'
-      }),
-      alert: {
-        get () {
-          return this.status
-        },
-        set (value) {
-          this.setAlert({
-            status : value,
-            type  : 'success',
-            text  : 'test',
-          })
-        }
+  name: 'Alert',
+  computed: {
+    ...mapGetters('alert', ['status', 'color', 'text']),
+    
+    alert: {
+      get() {
+        return this.status
       },
-    },
-    methods: {
-      ...mapActions({
-        setAlert  : 'alert/set',
-      }),
-      close(){
+      set(value) {
         this.setAlert({
-          status : false
+          status: value,
+          type: 'success', // default type (optional)
+          text: 'test'     // default text (optional)
         })
       }
     }
+  },
+  methods: {
+    ...mapActions('alert', ['setAlert']),
+    
+    close() {
+      this.setAlert({
+        status: false,
+        type: '',
+        text: ''
+      })
+    }
+  }
 }
 </script>
