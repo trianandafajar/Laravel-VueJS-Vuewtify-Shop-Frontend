305640042cbd2fd03e7976c9e3e33e9e50f03714
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-27 23:38:20 +0700

    Update Compinent Bookitem

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,32 +1,45 @@
 <template>
   <div>
-    <v-card :to="'/book/'+ book.slug">
+    <v-card :to="`/book/${book.slug}`" class="mb-4">
       <v-img
-        :src="getImage('/books/'+book.cover)"
+        :src="getImage(`/books/${book.cover}`)"
         class="white--text"
         height="200px"
       >
-        <v-card-title 
+        <v-card-title
           class="fill-height align-end"
           v-text="book.title"
-        ></v-card-title>
+        />
       </v-img>
 
       <v-card-actions>
-        <v-icon>mdi-cash</v-icon> 
-        <span class="orange--text"> Rp {{ book.price.toLocaleString('id-ID') }} </span>
+        <v-icon left>mdi-cash</v-icon>
+        <span class="orange--text">Rp {{ formatPrice(book.price) }}</span>
         <v-spacer></v-spacer>
-        <v-icon>mdi-eye</v-icon> {{ book.views }}
+        <v-icon left>mdi-eye</v-icon>
+        <span>{{ book.views }}</span>
       </v-card-actions>
-
     </v-card>
   </div>
 </template>
+
 <script>
 export default {
-  name: 'book-item',
-  props: ['book'],
-  data: () => ({
-  }),
+  name: 'BookItem',
+  props: {
+    book: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    getImage(path) {
+      // Sesuaikan kalau ada global base URL, atau sekadar return relative path
+      return path
+    },
+    formatPrice(price) {
+      return price.toLocaleString('id-ID')
+    }
+  }
 }
 </script>
